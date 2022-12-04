6cd3a8a038bb27c0d75de03dd302464a57c13893
Shengmao Li <shengmao.li1@gmail.com>
2022-12-04 07:50:40 +0100

    day4

---
+++ day4.py
@@ -0,0 +1,29 @@
+from itertools import permutations, combinations, chain, cycle, repeat, product
+from collections import defaultdict, Counter, deque, ChainMap
+from copy import deepcopy
+import os,math, difflib, pathlib, string, re
+
+cur_dir = pathlib.Path(__file__).resolve().parents[0] #.parents[0] #direcotry of the script being run
+file_name ="/input4.txt"
+inputs = open(str(cur_dir)+file_name).read().split("\n")
+
+lines = [[ list(map(int,l.split("-")))for l in  line.split(",")]for line in inputs]
+
+ans = 0 
+for line in lines:
+    first, second = line
+    if second[1] >= first[1] >= first[0] >= second[0]:
+        ans += 1
+    elif first[1] >= second[1] >= second[0] >= first[0]:
+        ans += 1
+
+print("part1: ", ans)
+
+
+ans = 0 
+for line in lines:
+    first, second = line
+    overlap = len(set(range(first[0], first[1]+1)) & set(set(range(second[0], second[1]+1)))) > 0
+    ans += overlap
+
+print("part2: ", ans)

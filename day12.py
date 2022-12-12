5ffdd339a9101d29367056c77b63dd6859c027ca
Shengmao Li <shengmao.li1@gmail.com>
2022-12-12 12:20:41 +0100

    day12 - clean up, add alternatives

--- day12.py
+++ day12.py
@@ -39,34 +39,71 @@ def get_neighbours(x,y):
     return [(r, c) for r,c in neighbours if 0 <= c < n and 0 <= r <m]
 
 
-def dijkstra( start, end):
+#use min_heap as classic dijstra approach does
+def dijkstra_( start, end):
     visited = set()
-    heap = [(0, start, visited)]
+    heap = [(0, start)]
     while heap:
-        cost, u, visited = heapq.heappop(heap)
         x,y = u
+        cost, u = heapq.heappop(heap)
         if u in visited:
             continue
         visited.add(u)
-        # print("lines[x][y]", lines[x][y], cost, u)
         if lines[x][y] == "E":
             return cost
 
         for cx,cy in get_neighbours(x,y):
             if (cx,cy) in visited:
                 continue            
-            # print(height_map)
-            # print(m,n, cx,cy, x,y, height_map[cx][cy])
             if height_map[cx][cy] - height_map[x][y] > 1:
                 continue 
-            next = cost + 1
-            heapq.heappush(heap, (next, (cx,cy), visited))
+            heapq.heappush(heap, (cost + 1, (cx,cy)))
     return float("inf")
 
 
+#queue can be used as well since the dis between two neighbouring postions is 1  
+def dijkstra( start, end):
+    visited = set()
+    queue = deque()
+    queue.appendleft((0, start))
+    while queue:
+        cost, u = queue.pop()
+        x,y = u
+        if u in visited:
+            continue
+        visited.add(u)
+        if lines[x][y] == "E":
+            return cost
+        for cx,cy in get_neighbours(x,y):
+            if (cx,cy) in visited:
+                continue            
+            if height_map[cx][cy] - height_map[x][y] > 1:
+                continue 
+            queue.appendleft((cost + 1, (cx,cy)))
+    return float("inf")
+
+#use queue plus distance
+def dijkstra( start, end):
+    dist = [[float("inf") for __ in range(n)] for _ in range(m)]
+    dist[start[0]][start[1]] = 0 
+    queue = deque()
+    queue.appendleft(start)
+    while queue:
+        x,y = queue.pop()        
+        if lines[x][y] == "E":
+            return dist[x][y]
+        for cx,cy in get_neighbours(x,y):
+            if height_map[cx][cy] - height_map[x][y] > 1:
+                continue 
+            if dist[x][y] + 1 >= dist[cx][cy]:
+                continue
+            dist[cx][cy] = dist[x][y] + 1
+            queue.appendleft((cx,cy))
+    
+    return float("inf")
+
 steps = dijkstra(start,end)
 print("part1: ", steps)
-
 res = float("inf")
 
 for x, line in enumerate(lines):
